9134a22ac59391d4e8a643320770c9f2971913e4
Sarah-Soares <sarahsoares@alu.ufc.br>
2021-08-19 19:55:01 -0300

    Inserts_Atualizados
    Atualizei os inserts, removi os id's e alterei o nome da tabela no insert "log interação" para "log alterações"

--- Scripts Sql/inserts.sql
+++ Scripts Sql/inserts.sql
@@ -24,48 +24,48 @@ values('Juan Pimentel', '2001-11-13', '111111111-11', 'juanpimentel', '12345', '
         ('Sarah Soares', '2002-01-23', '22222222-22', 'sarahsoares', '122333', 'DIR', 2), 
         ('Marcus Queiros', '2002-10-17', '3333333-33', 'marcusqueiros', '3333333', 'GER', 3), 
         ('Ana Beatriz', '2002-03-11', '44444444-44', 'anabeatriz', '444444', 'FUN', 4), 
-        ('Yara Paula', '2002-06-09', '55555555-55', 'yarapaula', '555555', 'EST', 5)
+        ('Yara Paula', '2002-06-09', '55555555-55', 'yarapaula', '555555', 'EST', 5);
 
 --TABELA LOG INTERACAO
-insert into log_interacao (data, tipo, codigo, mensagem, login_pessoa)
-values ('2021-08-17', 'INS', 'abcD12e', 'Bem-vindo', 'juanpimentel')
+insert into log_alteracao (data, tipo, codigo, mensagem, login_pessoa)
+values ('2021-08-17', 'INS', 'abcD12e', 'Bem-vindo', 'juanpimentel');
 
 --TABELA DONO
 insert into dono (cpf)
-values ('111111111-11')
+values ('111111111-11');
 
 --TABELA EMPRESA 
 insert into empresa(orcamento, cnpj, nome, cpf_dono)
-values(100.000, '1234578-0001', 'bmd.sy', '111111111-11')
+values(100.000, '1234578-0001', 'bmd.sy', '111111111-11');
 
 --TABELA CATEGORIA
 insert into categoria (nome, carga_horaria, descricao, salario, cnpj_empresa)
-values('Programador', 08, 'Desenvolvedores de programas de computadores', 10.000, '1234578-0001')
+values('Programador', 08, 'Desenvolvedores de programas de computadores', 10.000, '1234578-0001');
 
 --TABELA SETOR
-insert into setor(id, nome, orcamento, cnpj_empresa)
-values (092021, 'Programacao', 50.000, '1234578-0001')
+insert into setor(nome, orcamento, cnpj_empresa)
+values ('Programacao', 50.000, '1234578-0001');
 
 --TABELA FUNCIONARIO
 insert into funcionario(bonificacao, cpf, id_categoria, id_setor, dia_pagamento, data_inicio)
-values (1.500, '44444444-44', 082021, 092021, 01, '2021-08-01'), (5.000, '3333333-33', 082021, 092021, 01, '2020-10-17')
+values (1.500, '44444444-44', 1, 1, 01, '2021-08-01'), (5.000, '3333333-33', 1, 1, 01, '2020-10-17');
 
 --TABELA DIRETOR
 insert into diretor (cpf, cnpj_empresa, id_categoria)
-values('22222222-22', '1234578-0001', 082021)
+values('22222222-22', '1234578-0001', 1);
 
 --TABELA ESTAGIARIO
 insert into estagiario(cpf, id_categoria, id_setor, dia_pagamento, inicio_estagio, tempo_estagio)
-values('55555555-55', 082021, 092021, 01, '2021-08-10', 90)
+values('55555555-55', 1, 1, 01, '2021-08-10', 90);
 
 --TABELA TREINA
 insert into treina (cpf_estagiario, cpf_funcionario, carga_horaria_treinamento)
-values ('55555555-55', '44444444-44', 06)
+values ('55555555-55', '44444444-44', 06);
 
 --TABELA GERENTE
 insert into gerente(bonificacao_gerente, cpf)
-values(5.000, '3333333-33')
+values(5.000, '3333333-33');
 
 --TABELA GERENCIA
 insert into gerencia(id_setor, gerente_cpf, data_inicio)
-values (092021, '3333333-33', '2020-10-17')
+values (1, '3333333-33', '2020-10-17');
