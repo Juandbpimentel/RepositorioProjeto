f5b4849aeed3440c08c43098e815eb2a640c489d
Marcus Davi S. Queiros <marcusdavi.soar@gmail.com>
2021-08-21 21:09:39 -0300

    Update Estagiario.java

--- projeto_poo/src/modelos/usuarios/Estagiario.java
+++ projeto_poo/src/modelos/usuarios/Estagiario.java
@@ -1,4 +1,6 @@
 package modelos.usuarios;
+import interfaces.alterDB.GerenteAlterDB;
+import interfaces.gui.GerenteGUI;
 import modelos.empresa.Categoria;
 import modelos.lugar.Endereco;
 import sistema.Conexao;
@@ -15,7 +17,86 @@ public class Estagiario extends Pessoa implements EstagiarioGUI {
 
 
     public Estagiario(String nome, String login, String senha, String tipo, String cpf, LocalDate dataNasc,/* Endereco endereco, Conexao conexao,*/ Timestamp inicioEstagio, int tempoEstagio, int diaPagamento/*, Categoria categoria*/) {
-        super(nome, login, senha, tipo, cpf, dataNasc);
+        package modelos.usuarios;
+
+import modelos.empresa.Categoria;
+import modelos.lugar.Endereco;
+import sistema.Conexao;
+import interfaces.alterDB.GerenteAlterDB;
+import interfaces.gui.GerenteGUI;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+        public class Gerente extends Funcionario implements GerenteGUI, GerenteAlterDB {
+            private double bonificacaoGerente;
+
+
+            public Gerente(String nome, String login, String senha, String tipo, String cpf, LocalDate dataNasc, int idCategoria, int idSetor, int diaPagamento, LocalDate dataInicio, double bonificacao, double bonificacaoGerente, int idEndereco) {
+                super(nome,login,senha,tipo,cpf,dataNasc,idCategoria,idSetor,diaPagamento,dataInicio,bonificacao,idEndereco);
+                this.bonificacaoGerente = bonificacaoGerente;
+            }
+
+            /**
+             * GUI
+             */
+            @Override
+            public vo   @Override
+            public void adminisonsultarFuncionarios() {
+
+            }
+
+            @Override
+            public void consultarEstagiarios() {
+
+            }
+
+            /**
+             * AlterDB
+             */
+            @Override
+            public void demitirFuncionario() {
+
+            }
+
+            @Override
+            public void bonificarFuncionario() {
+
+            }
+
+            @Override
+            public void admitirFuncionario() {
+
+            }
+
+            @Override
+            public void admitirEstagiario() {
+
+            }
+
+            @Override
+            public void dispensarEstagiario() {
+
+            }
+
+            @Override
+            public void trocarEstagiarioSetor() {
+
+            }
+
+            //Getters e Setters
+
+            public double getBonificacaoGerente() {
+                return bonificacaoGerente;
+            }
+
+            public void setBonificacaoGerente(double bonificacaoGerente) {
+                this.bonificacaoGerente = bonificacaoGerente;
+            }
+
+
+        }
+
         this.inicioEstagio = inicioEstagio;
         this.tempoEstagio = tempoEstagio;
         this.diaPagamento = diaPagamento;
