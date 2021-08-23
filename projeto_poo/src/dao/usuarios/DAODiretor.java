07595cc2832a5112af3a6e3f91c986fe7df42a9e
Sarah-Soares <sarahsoares@alu.ufc.br>
2021-08-23 19:56:18 -0300

    DAODiretor

--- projeto_poo/src/dao/usuarios/DAODiretor.java
+++ projeto_poo/src/dao/usuarios/DAODiretor.java
@@ -56,6 +56,49 @@ public class DAODiretor {
             System.err.println("Erro Geral: " + erroGeral);
             return null;
         }
+    }   
+
+
+
+
+
+    public Diretor readOneDiretor(String cpf){
+
+        try{
+            conexao.conect();
+            String sqlQueryDiretor = "Select * from DIRETOR where cpf = \'"+cpf+"\'";
+            ResultSet resultadoQueryDiretor = conexao.executaQuery(sqlQueryDiretor);
+            if(!resultadoQueryDiretor.next()){
+                throw new NullPointerException("O Diretor que você está procurando não foi encontrado, retornou nulo");
+            }else{ 
+                String cnpj_empresa = resultadoQueryDiretor.getString("cnpj_empresa");
+                int id_categoria = resultadoQueryDiretor.getInt("id_categoria");
+
+                String sqlQueryPessoa = "select * from pessoa where cpf = \'"+cpf+"\'";
+                ResultSet resultadoQueryPessoa = conexao.executaQuery(sqlQueryPessoa);
+                if (!resultadoQueryPessoa.next()){
+                    throw new NullPointerException("O Dono que você está procurando não foi encontrado, retornou nulo");
+                }else{
+                    String  nome = resultadoQueryPessoa.getString("nome"),
+                            login = resultadoQueryPessoa.getString("login"),
+                            senha = resultadoQueryPessoa.getString("senha"),
+                            tipo = resultadoQueryPessoa.getString("tipo");
+                    Date data_nasc  = resultadoQueryPessoa.getDate("data_nasc");
+                    int id_endereco = resultadoQueryPessoa.getInt("id_endereco");
+
+                    Diretor diretor = new Diretor(nome, login, senha, tipo, cpf, data_nasc.toLocalDate(),cnpj_empresa, id_categoria, id_endereco);
+                    return diretor;
+
+                }
+                
+            }
+        }catch (SQLException erroSQL) {
+            System.err.println("Ocorreu um erro durante a busca do banco de dados: " + erroSQL);
+            return null;
+        } catch (Exception erroGeral) {
+            System.err.println("ocorreu um erro Geral: " + erroGeral);
+            return null;
+        }
     }
     public boolean deleteDiretor(String cpf){
         try{
@@ -76,5 +119,23 @@ public class DAODiretor {
             return false;
         }
         return false;
+    }
+    public boolean insertDiretor(Diretor diretor){
+        try{
+
+            conexao.conect();
+            String sqlInsertDiretor = "insert into public.Diretor(cpf, cnpj_empresa, id_categoria)\n"
+                                 + "values (\'"+diretor.getCpf()+"\' , \'"+diretor.getCnpj_empresa()+"\' , "+diretor.getId_categoria()+ ")\n";
+            int resultado = conexao.executaSql(sqlInsertDiretor);
+            return (resultado != 0);
+        } catch(SQLException e){
+            System.err.println("Houve um erro durante a inserção no banco de dados: "+e);
+            return false;
+        }catch(Exception e){
+            System.err.println("Houve um erro geral: "+e);
+            return false;
+
+        }
+
     }
 }
